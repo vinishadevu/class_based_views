170411af719eb3ce5f627be8ef30afc46ae23682
vinishadevu <vinishadevu888@gmail.com>
2023-05-22 18:07:50 +0530

    first commit

---
+++ app/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+from django.urls import reverse
+
+# Create your models here.
+class School(models.Model):
+    name=models.CharField(max_length=100) 
+    principal=models.CharField(max_length=100)  
+    location=models.CharField(max_length=100)
+
+    def __str__(self) -> str:
+        return self.name
+
+         
+    def get_absolute_url(self):
+        return reverse('detail',kwargs={'pk':self.pk})
+
+class Student(models.Model):
+    school=models.ForeignKey(School,on_delete=models.CASCADE,related_name='students')
+    sname=models.CharField(max_length=100)
+    sage=models.IntegerField()
+    def __str__(self) -> str:
+        return self.sname
+
+
+    
---
+++ app/views.py
@@ -0,0 +1,28 @@
+
+from django.shortcuts import render
+from app.models import *
+# Create your views here.
+from django.views.generic import DeleteView,UpdateView,TemplateView,ListView,DetailView,CreateView
+from django.urls import reverse_lazy
+class Home(TemplateView):
+    template_name='app/home.html'
+
+class SchoolList(ListView):
+    model = School
+    context_object_name='schools'
+
+class SchoolDetail(DetailView):
+    model=School
+    context_object_name='sclobject'
+class SchoolCreate(CreateView):
+    model=School
+    fields='__all__'
+class SchoolUpdate(UpdateView):
+    model=School
+    fields='__all__'
+class Schooldelete(DeleteView):
+    model=School
+    context_object_name='schoolobject'
+    success_url=reverse_lazy('SchoolList')
+
+
